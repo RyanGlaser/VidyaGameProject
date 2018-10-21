813ee5d1527abda1add81f037c7edb6ebf5e4b01
RyoTheMagicMan <ryanglaser150>
2018-10-20 20:45:01 -0500

    Added fireball projectile
    can now fire fireball projectiles with player using mouse to aim and left click to fire

---
+++ FinalProject/Assets/Scripts/CastSpell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastSpell : MonoBehaviour
+{
+
+    public GameObject spell;
+    private Transform playerPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerPos = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(spell, playerPos.position, Quaternion.identity);
+        }
+	}
+}
---
+++ FinalProject/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Vector2 target;
+    public float speed; 
+
+	// Use this for initialization
+	void Start ()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(Vector2.Distance(transform.position, target) < 0.001f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
