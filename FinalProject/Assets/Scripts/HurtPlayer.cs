2bd109b72af2bd3c395f98d061e3bad440b48f75
RyoTheMagicMan <ryanglaser150>
2018-10-27 18:10:11 -0500

    Added Player Health Script

---
+++ FinalProject/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    public int damageAmount;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.name == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHealthManager>().DamagePlayer(damageAmount);
+        }
+    }
+}
---
+++ FinalProject/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public int playerMaxHealth;
+    public int playerCurrentHealth;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetMaxHealth();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(playerCurrentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    public void DamagePlayer(int damageAmt)
+    {
+        playerCurrentHealth -= damageAmt;
+    }
+
+    public void SetMaxHealth()
+    {
+        playerCurrentHealth = playerMaxHealth;
+    }
+}
