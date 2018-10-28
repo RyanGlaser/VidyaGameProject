5657278e31fc95cdc088d364817dd8e359821812
RyoTheMagicMan <ryanglaser150>
2018-10-28 03:33:53 -0500

    added Network Health
    added skeleton of scripts for network health manager, and network projectile damage

---
+++ FinalProject/Assets/Scripts/NetworkHealthManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkHealthManager : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth;
+	// Use this for initialization
+	void Start ()
+    {
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void DamagePlayer(int dmgAmt)
+    {
+        currentHealth -= dmgAmt;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead: ");
+        }
+    }
+}
---
+++ FinalProject/Assets/Scripts/NetworkProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkProjectile : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<NetworkHealthManager>();
+        if(health != null)
+        {
+            health.DamagePlayer(10);
+        }
+
+        Destroy(gameObject);
+    }
+
+}
